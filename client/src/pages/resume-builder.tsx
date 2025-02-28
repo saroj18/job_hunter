3a98fcb7ca5233e474757c40b9c3fe01c2d816a9
bikalpa3D <vikalpradhanbca078@chitwancollege.edu.np>
2025-02-28 15:33:02 +0545

    basic info formdone

--- client/src/pages/resume-builder.tsx
+++ client/src/pages/resume-builder.tsx
@@ -1,7 +1,60 @@
+import { useState } from "react";
+import InfoStep from "../components/multi-step-form/info-step";
+import type { ResumeData } from "../types/resume";
 
+const initialFormData: ResumeData = {
+    title: "",
+    summary: "",
+    workExperience: [
+      {
+        jobTitle: "",
+        company: "",
+        location: "",
+        startDate: "",
+        endDate: "",
+        responsibilities: "",
+      },
+    ],
+    education: [
+      {
+        degree: "",
+        institution: "",
+        location: "",
+        graduationDate: "",
+      },
+    ],
+    skills: [""],
+    certifications: [
+      {
+        name: "",
+        issuingOrganization: "",
+        dateObtained: "",
+      },
+    ],
+    projects: [
+      {
+        title: "",
+        description: "",
+        link: "",
+      },
+    ],
+    languages: [""],
+  }
+  
 
 function ResumeBuilder() {
-  return <div>ResumeBuilder</div>;
+    const [formData, setFormData] = useState<ResumeData>(initialFormData)
+
+    const updateFormData = (stepData: Partial<ResumeData>) => {
+        setFormData((prev) => ({ ...prev, ...stepData }))
+      }
+    
+  return (
+    <div>
+      ResumeBuilder
+      <InfoStep data={formData}  updateData={updateFormData}/>
+    </div>
+  );
 }
 
 export default ResumeBuilder;
