fd2ea43f351ba809ab521a7884420098a5e77f46
ThamanGurung01 <tgurung519@gmail.com>
2025-02-28 14:06:18 +0545

    user.ts

---
+++ server/model/user.ts
@@ -0,0 +1,76 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  roles: ("user" | "admin")[];
+  location?: string;
+  linkedin?: string;
+  github?: string;
+  portfolio?: string;
+  refreshToken?: string;
+  isPasswordCorrect(password: string): Promise<boolean>;
+  generateAccessToken(): string;
+  generateRefreshToken(): string;
+}
+
+const UserSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    roles: {
+      type: [String],
+      required: true,
+      default: ["user"],
+      enum: ["user", "admin"],
+    },
+    location: { type: String },
+    linkedin: { type: String },
+    github: { type: String },
+    portfolio: { type: String },
+    refreshToken: { type: String },
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+UserSchema.methods.isPasswordCorrect = async function (password: string): Promise<boolean> {
+  return bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateAccessToken = function (): string {
+  return jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET as string,
+    { expiresIn: "1d" }
+  );
+};
+
+UserSchema.methods.generateRefreshToken = function (): string {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET as string,
+    { expiresIn: "7d" }
+  );
+};
+
+const User: Model<IUser> = mongoose.model<IUser>("User", UserSchema);
+
+export { User, IUser };
